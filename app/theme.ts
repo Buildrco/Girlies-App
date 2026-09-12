e71cd6e7eaa2c5ba19737884f0ecf6b033fcd55d
Buildrco <dbanful.tqmc@gmail.com>
2026-09-12 09:30:32 +0000

    Add files via upload

---
+++ app/theme.ts
@@ -0,0 +1,2 @@
+export const C={bg:'#FFF9FA',paper:'#FFFFFF',ink:'#171318',muted:'#716A70',line:'#EDE4E8',pink:'#F64D86',rose:'#FFB7C9',coral:'#FF705E',sun:'#FFD45A',lilac:'#B89CFF',plum:'#5D3158',mint:'#8EDBC2',blue:'#4E72FF',cream:'#FFF0D8',green:'#25B77A',red:'#F0445B',black:'#101012'};
+export const R={sm:16,md:24,lg:32,xl:42};
