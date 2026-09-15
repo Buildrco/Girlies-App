ef28e6def2d67b51e38ba744e9808c5735ea4f4f
Buildrco <dbanful.tqmc@gmail.com>
2026-09-15 18:09:39 +0000

    Fix native stack navigation option

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -19,7 +19,6 @@ export default function Layout() {
         screenOptions={{
           headerShown: false,
           freezeOnBlur: false,
-          detachInactiveScreens: false,
         }}
       >
         <Stack.Screen name="home" options={{ animation: 'none' }} />
